d151d2e4b62d037f6cffaab551a48024a999d6cc
Галима <@DESKTOP-PGIEF0N>
2023-04-15 19:00:14 +0300

    Header initialization DONE

--- String/main.cpp
+++ String/main.cpp
@@ -25,32 +25,32 @@ public:
 		return str;
 	}
 	//            Constructors:
-	explicit String(int size = 256)
+	explicit String(int size = 256):size(size), str(new char[size] {})
 	{
-		this->size = size;
-		this->str = new char[size] {};
+		//this->size = size;
+		//this->str = new char[size] {};
 		cout << "Default1ArgConstruct:\t" << this << endl;
 	}
-	String(const char* str)
+	String(const char* str):size(strlen(str) + 1), str(new char[size] {})
 	{
-		this->size = strlen(str)+1;
-		this->str = new char[size] {};
+		//this->size = strlen(str)+1;
+		//this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other):size(other.size), str(new char[size] {})
 	{
 		//Deep copy (Побитовое копирование):
-		this->size = other.size;
-		this->str = new char[size] {};
+		//this->size = other.size;
+		//this->str = new char[size] {};
 		for(int i = 0; i<size; i++)this->str[i]= other.str[i];
 		cout << "CopyConstructor:\t" << this << endl;
 	}
-	String(String&& other)
+	String(String&& other):size(other.size), str(other.str)
 	{
 		//Shallow copy (Поверхностное копирование):
-		this->size = other.size;
-		this->str = other.str;
+		//this->size = other.size;
+		//this->str = other.str;
 		other.size = 0;
 		other.str = nullptr;
 		cout << "MoveConstructor:\t" << this << endl;
