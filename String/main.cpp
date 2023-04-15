ee97bb36c75978e226f0f20208dca653891815c0
Галима <@DESKTOP-PGIEF0N>
2023-04-15 19:25:07 +0300

    Delegation DONE

--- String/main.cpp
+++ String/main.cpp
@@ -31,19 +31,19 @@ public:
 		//this->str = new char[size] {};
 		cout << "Default1ArgConstruct:\t" << this << endl;
 	}
-	String(const char* str):size(strlen(str) + 1), str(new char[size] {})
+	String(const char* str):String(strlen(str)+1)//Делегируем выделение памяти конструктору по умолчанию
 	{
 		//this->size = strlen(str)+1;
 		//this->str = new char[size] {};
 		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	String(const String& other):size(other.size), str(new char[size] {})
+	String(const String& other):String(other.str)
 	{
 		//Deep copy (Побитовое копирование):
 		//this->size = other.size;
 		//this->str = new char[size] {};
-		for(int i = 0; i<size; i++)this->str[i]= other.str[i];
+		//for(int i = 0; i<size; i++)this->str[i]= other.str[i];
 		cout << "CopyConstructor:\t" << this << endl;
 	}
 	String(String&& other):size(other.size), str(other.str)
